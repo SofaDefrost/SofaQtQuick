7923be9ffe6db0f122843bf3f578042a4a6dcf3e
Bruno Marques <bmarques68@gmail.com>
2020-09-28 12:41:14 +0200

    FIX LD_LIBRARY_PATH

--- applications-dev/projects/runSofa2/runSofa2.sh
+++ applications-dev/projects/runSofa2/runSofa2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export PYTHONPATH=../lib/python3/site-packages
-export LD_LIBRARY_PATH=../lib:x86_64-linux-gnu/
 export PYTHONHOME=../
+export PYTHONPATH=../lib/python3/site-packages
+export LD_LIBRARY_PATH=../lib:../lib/x86_64-linux-gnu/
 
 ./runSofa2
